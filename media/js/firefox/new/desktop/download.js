4977d1b5bb41f5a8082be20e03ffd9f800fd56b4
Stephanie Hobson <sjhobson@gmail.com>
2020-05-26 15:54:48 -0700

    First pass

---
+++ media/js/firefox/new/desktop/download.js
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+(function() {
+    'use strict';
+
+    // move mobile banner up and reveal
+
+    // animation
+
+
+    let observer = new IntersectionObserver(
+        (entries, observer) => {
+        entries.forEach(entry => {
+           if (entry.isIntersecting) {
+                  entry.target.classList.add('fancy');
+                  //entry.unobserve();
+                }
+            });
+        },);
+
+        //TODO: check for intersection observer support (and naimation support?)
+        document.querySelectorAll('.js-go').forEach(element => {
+          var rect = element.getBoundingClientRect();
+          var vh = window.innerHeight;
+          // TODO: need to take scroll height into account
+          // don't want stuff to animate while they're reading
+          if(rect.top > vh) {
+            element.classList.add('ani');
+            observer.observe(element)
+          }
+        });
+
+    //
+
+})();
